7ce33629b5388b30fe199c8fccf742831e8e471f
kayyon <89946346+kayyon@users.noreply.github.com>
2022-03-21 23:14:01 -0400

    added stylesheet

--- project2.php
+++ project2.php
@@ -5,11 +5,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Wordle with PHP</title>
 </head>
 
 <body>
+    <h1>Welcome to Wordle with PHP</h1>
 
+    <php? ?>
 </body>
 
 </html>
